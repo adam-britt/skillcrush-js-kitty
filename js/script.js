22f44646d94c14581d76b9083337ae05d5a8bf78
Adam Britt <adamrussellbritt@gmail.com>
2023-07-14 15:54:28 -0500

    button click events

--- js/script.js
+++ js/script.js
@@ -8,8 +8,12 @@ button.addEventListener("click", function() {
     if(cat.classList.contains("show")) {
         // console.log("Yes");
         cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
     } else {
         // console.log("No");
         cat.classList.add("show");
+        button.innerText = "Shoo, cat!!";
+        button.classList.remove("disappear");
     }
 });
