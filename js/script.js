b82ef41f41a8217d34bc46fcc1cc7806efe2a3b0
Adam Britt <adamrussellbritt@gmail.com>
2023-07-14 15:24:30 -0500

    final edits

--- js/script.js
+++ js/script.js
@@ -5,6 +5,6 @@ var cat = document.querySelector(".cat")
 console.log(cat);
 
 button.addEventListener("click", function(){
-    console.log("It's working!")
+    // console.log("It's working!")
     cat.classList.add("show")
 });
